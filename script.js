aee2e5ba215bf542aaadf68ddfc2341b56e0aa5c
Ufe Bassey <114451638+Ufeabasi@users.noreply.github.com>
2024-10-29 13:33:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -386,17 +386,8 @@ function displayUserData(userData) {
   const content = document.getElementById('user_name');
 
   if (userData) {
-    content.innerHTML = `
-      <h1 class="text-2xl font-bold mb-4">User Data</h1>
-      <ul>
-        <li>ID: ${userData.id}</li>
-        <li>First Name: ${userData.first_name}</li>
-        <li>Last Name: ${userData.last_name || 'N/A'}</li>
-        <li>Username: ${userData.username || 'N/A'}</li>
-        <li>Language Code: ${userData.language_code}</li>
-        <li>Is Premium: ${userData.is_premium ? 'Yes' : 'No'}</li>
-      </ul>
-    `;
+    content.innerHTML = userData.first_name + userData.username;
+    
   } else {
     content.innerHTML = 'User data not available.';
   }
